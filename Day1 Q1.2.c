3bc119d352d317c3952e21e657e8aa5efe3b45f3
Sahyog7277 <sahyog3141@gmail.com>
2026-08-26 00:15:46 +0530

    Add files via upload

---
+++ Day1 Q1.2.c
@@ -0,0 +1,9 @@
+// This program calculates the sum of two integers and prints the result.
+#include <stdio.h>
+
+int main(void) {
+    int num1 = -1;
+    int num2 = 20;
+    printf("Sum = %d\n", num1 + num2);
+    return 0;
+}
