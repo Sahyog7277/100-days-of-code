7792f60cbf04368abd09c4585b5be8585f04e659
Sahyog7277 <sahyog3141@gmail.com>
2026-08-26 00:23:23 +0530

    Add files via upload

---
+++ Day1 Q2.1.c
@@ -0,0 +1,20 @@
+// This program calculates the sum, difference, product, and quotient of two floating-point numbers and prints the results.
+#include <stdio.h>
+
+int main(void) {
+    float num1 = 10;
+    float num2 = 2;
+
+    printf("Sum = %.2f\n", num1 + num2);
+    printf("Difference = %.2f\n", num1 - num2);
+    printf("Product = %.2f\n", num1 * num2);
+    printf("Quotient = %.2f\n", num1 / num2);
+
+    return 0;
+    
+
+
+
+
+
+}
